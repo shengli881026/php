10da8b0699c43c936b03e2e6976e094bcdcbade0
shengli881026 <shengli881026@gmail.com>
2014-12-21 21:20:53 +0800

    session add
    sessionadd

---
+++ session.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+//SID 预定义常量 windows 下使用。
+/**
+ * 当客户端禁用 cookie 使,
+ 在linux 和Unix下使用下面的方法:
+ 1、设置php.ini中的session.use_trans_sid = 1或者编译时打开打开了--enable-trans-sid选项，让PHP自动跨页传递session id。
+ 2、手动通过URL传值、隐藏表单传递session id。
+ 3、用文件、数据库等形式保存session_id,在跨页过程中手动调用。
+*/
+
+$s_name = session_name();
+if(empty($_SESSION['username']) || empty($_COOKIE[$s_name]))
+{
+	echo '需要重新登陆';
+	$u ='admin';
+	$tmp_pwd ='admin';
+	$username = !empty($_REQUEST['username']) ? $_REQUEST['username'] : '';
+	$pwd = !empty($_REQUEST['pwd']) ? $_REQUEST['pwd'] : '';
+	//var_dump($username,$pwd);exit;
+	$is_set = !empty($_REQUEST['is_set']) ? $_REQUEST['is_set'] : 0;
+	if($username ==$u && $pwd ==$tmp_pwd)
+	{
+		//$lifetime =10;
+		//setcookie(session_name(),session_id(),time()+10,"/");
+		if(SID=='')
+		{
+			echo 'sid ---------set cookie';
+			setcookie(session_name(),session_id(),time()+10,"/");
+		}else{
+			echo 'sid --------set session';
+			$lifetime=10;//保存1分钟
+			session_set_cookie_params($lifetime);
+			session_regenerate_id(true);
+		}
+		//print_r(session_get_cookie_params());
+		$_SESSION['username'] = $username;
+		$_SESSION['pwd'] = $pwd;
+		//var_dump($_REQUEST);
+		//echo '';
+	}
+}else{
+	echo '登陆成功';exit;
+}
+?>
+<!DOCTYPE html> 
+<html>
+<head>
+<!--STATUS OK-->
+<meta http-equiv=Content-Type content="text/html; charset=gb2312">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+<title>-----------</title>
+<script type="text/javascript" src="jquery-1.7.1.min.js"></script>
+</head>
+<body>
+<script type="text/javascript">
+
+</script>
+<form action="session.php" method="post">
+ username:<input type="text" name="username" value=''/><br/>
+ password:<input type="password" name="pwd" value='' /></br/>
+ 下次自动登陆:<input type="checkbox" name="is_set" value="1"/><br/>
+ 提交:<input type="submit" name="login" value="提交"/>---<input type="reset" name="reset" value="重置"/>
+</form>
+</body>
+</html>
